9aedf816015031ef29aed26af0ef3b49b73de094
Chris Castle <crcastle@gmail.com>
2015-12-08 01:06:01 -0800

    Begin authentication code

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import Home from './components/Home.vue'
 import ScheduleTweet from './components/ScheduleTweet.vue'
 import TweetView from './components/TweetView.vue'
 
-// install router
+// install plugins
 Vue.use(Router)
 
 // register filters globally
@@ -22,15 +22,29 @@ router.map({
     component: Home
   },
   '/tweet': {
-    component: ScheduleTweet
+    component: ScheduleTweet,
+    auth: true
   },
   '/scheduled': {
-    component: TweetView
+    component: TweetView,
+    auth: true
   }
 })
 
+// always scroll to the top when changing page
 router.beforeEach(function () {
   window.scrollTo(0, 0)
+})
+
+// check if user is authenticated on certain pages
+router.beforeEach(function (transition) {
+  // if (transition.to.auth) {
+    // if (isAuthenticated)
+      transition.next()
+    // } else {
+      // transition.abort()
+    //}
+  // }
 })
 
 router.redirect({
