3819821014860c474f047a0984628933977f2e33
Chris Castle <crcastle@gmail.com>
2015-12-12 14:49:37 -0800

    Move loading of <script>'s around

--- src/components/App.vue
+++ src/components/App.vue
@@ -39,24 +39,6 @@
       </div>
 
     </div>
-
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/lib/axios/dist/axios.standalone.js"></script>
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/lib/CryptoJS/rollups/hmac-sha256.js"></script>
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/lib/CryptoJS/rollups/sha256.js"></script>
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/lib/CryptoJS/components/hmac.js"></script>
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/lib/CryptoJS/components/enc-base64.js"></script>
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/lib/moment/moment.js"></script>
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/lib/url-template/url-template.js"></script>
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/lib/apiGatewayCore/sigV4Client.js"></script>
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/lib/apiGatewayCore/apiGatewayClient.js"></script>
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/lib/apiGatewayCore/simpleHttpClient.js"></script>
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/lib/apiGatewayCore/utils.js"></script>
-    <script type="text/javascript" src="static/js/apiGateway-js-sdk/apigClient.js"></script>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="static/js/jquery.min.js"><\/script>')</script>
-    <script src="static/js/bootstrap.min.js"></script>
-    <script src="static/js/ie10-viewport-bug-workaround.js"></script>
   </div>
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,7 +16,6 @@
     </div>
     <em>No server setup or ongoing administration.<br>An order of magnitude cheaper than an EC2 micro instance!</em><br>
     <a href="https://github.com/crcastle/serverless-buffer-app#readme" class="btn btn-lg btn-default">Learn More</a>
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
   </div>
 </template>
 
