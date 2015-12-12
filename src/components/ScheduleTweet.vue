be5bda5d7b7a4b0982e144f54941279522cb8609
Chris Castle <crcastle@gmail.com>
2015-12-11 17:51:10 -0800

    Add tweet date scheduling functionality

--- src/components/ScheduleTweet.vue
+++ src/components/ScheduleTweet.vue
@@ -5,10 +5,12 @@
               placeholder="What's the happs?"
               v-model="newTweet"
               rows="8" cols="40"></textarea>
-    <button id="schedule-tweet" v-on:click="scheduleTweet">Show Modal</button>
+    <input id="datetimepicker" size="16"
+              type="text" v-model="date">
+    <button class="btn btn-primary" id="schedule-tweet" v-on:click="scheduleTweet">Schedule Tweet</button>
 
 
-
+    <!-- TODO: move modal to App.vue -->
     <!-- use the modal component, pass in the prop -->
     <modal :show.sync="showModal">
       <!--
@@ -36,20 +38,41 @@ export default {
     return {
       showModal: false,
       newTweet: '',
+      date: null,
       error: null,
       success: null
+    }
+  },
+
+  computed: {
+    isoDate: function() {
+      return new Date(this.date).toISOString()
+    },
+    millisecondsUtcDate: function() {
+      return new Date(this.date).valueOf()
     }
   },
 
   methods: {
     scheduleTweet: function(event) {
       let that = this
-      this.$root.store.createNewScheduledTweet(this.newTweet, function(err, response) {
-        if (err) { console.log('Error creating tweet'); console.log(err); }
-        console.log(response)
+      this.$root.store.createNewScheduledTweet(this.newTweet, this.millisecondsUtcDate, function(err, response) {
+        if (err) {
+          console.error('Error creating tweet');
+          console.error(err);
+          return;
+        }
+
+        console.info(response)
         that.newTweet = ''
       })
     }
+  },
+  ready: () => {
+    $('#datetimepicker').datetimepicker({
+      showMeridian: true,
+      minuteStep: 15
+    })
   }
 }
 </script>
