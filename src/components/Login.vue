b21eafe29d1b2dca5747302a6727ac06c9b77be4
Chris Castle <crcastle@gmail.com>
2015-12-12 16:43:00 -0800

    Add login Vue

---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div id="login"></div>
+  </div>
+</template>
+
+<script>
+// console.log(gapi)
+export default {
+  name: 'Login',
+  methods: {
+    onSuccess(googleUser) {
+      console.log('*** User authenticated successfully')
+      this.$dispatch('logged-in', googleUser)
+    },
+    onFailure(error) {
+      console.log('*** User authentication failed')
+      this.$dispatch('logged-out')
+    }
+  },
+
+  ready() {
+    window.gapi.signin2.render('login', {
+      'scope': 'https://www.googleapis.com/auth/plus.login',
+      // 'width': 250,
+      // 'height': 50,
+      // 'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': this.onSuccess,
+      'onfailure': this.onFailure
+    });
+  }
+}
+</script>
