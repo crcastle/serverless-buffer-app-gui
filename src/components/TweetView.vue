340afa11911817eaf52a01da7e012415dfc3b7b6
Chris Castle <crcastle@gmail.com>
2015-12-13 23:39:43 -0800

    Use Grid component within TweetView

--- src/components/TweetView.vue
+++ src/components/TweetView.vue
@@ -1,13 +1,39 @@
 <template>
-  <div></div>
+  <div id="grid">
+    <form id="search">
+      Search <input name="query" v-model="searchQuery">
+    </form>
+    <grid
+      :data="gridData"
+      :columns="gridColumns"
+      :filter-key="searchQuery">
+    </grid>
+  </div>
 </template>
 
 <script>
+import Grid from './Grid.vue'
 export default {
   name: 'TweetView',
+  components: {
+    Grid
+  },
+
   data: () => {
     return {
+      searchQuery: '',
+      gridColumns: ['postOn', 'created', 'text'],
+      gridData: [
+        { postOn: 'hi', created: 'bye', text: 'hello'},
+        { postOn: 'yo', created: 'asdf', text: 'lkjh'}
+      ]
     }
   }
 }
 </script>
+
+<style>
+#grid {
+  text-align: center;
+}
+</style>
