b034b4559d7a8f9503eaf62c03cd4ed913834b31
Chris Castle <crcastle@gmail.com>
2015-12-19 21:43:14 -0800

    Show relative times in scheduled tweet table

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,20 +1,21 @@
 export function fromNow (time) {
-  const between = Date.now() / 1000 - Number(time)
+  let between = Date.now() / 1000 - Number(time)/1000
+  const suffix = (between > 0) ? ' ago' : ' from now'
+  between = Math.abs(between)
+
   if (between < 3600) {
-    return pluralize(~~(between / 60), ' minute')
+    return pluralize(~~(between / 60), ' minute') + suffix
   } else if (between < 86400) {
-    return pluralize(~~(between / 3600), ' hour')
+    return pluralize(~~(between / 3600), ' hour') + suffix
   } else {
-    return pluralize(~~(between / 86400), ' day')
+    return pluralize(~~(between / 86400), ' day') + suffix
   }
 }
 
 export function formatDate(value, separator) {
   if (typeof value != "number") return value
 
-  separator = (separator) ? separator : " "
-  const d = new Date(value)
-  return d.toLocaleDateString() + separator + d.toLocaleTimeString()
+  return fromNow(new Date(value))
 }
 
 function pluralize(time, label) {
